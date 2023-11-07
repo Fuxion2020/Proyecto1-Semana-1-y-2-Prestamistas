dd73a439415a89c3cb6c2dee6d9bc00be67090dd
ArianaBlancoF <122564390+ArianaBlancoF@users.noreply.github.com>
2023-11-07 17:08:31 -0400

    Add files via upload

--- ClassCliente.py
+++ ClassCliente.py
@@ -1,18 +1,16 @@
 import pickle
 from ClassPrestamo import * 
 
-
 class Cliente:
     def __init__(self, nombre, cedula, saldo):
         self.nombre = nombre
         self.cedula = cedula
         self.saldo = saldo
-        self.prestamos = {}  # Diccionario para guardar los préstamos 
+        self.prestamos = []  # lista para guardar los préstamos
 
     def solicitar_prestamo(self, monto, fecha):
-        """Crea un nuevo préstamo para el cliente y lo agrega al diccionario de préstamos."""
+        """Crea un nuevo préstamo para el cliente y lo agrega a la lista de préstamos."""
         prestamo = Prestamo(monto, fecha, self)
-        self.prestamos[self.cedula] = prestamo  
+        self.prestamos.append(prestamo)
         self.saldo += monto
         return prestamo
-
